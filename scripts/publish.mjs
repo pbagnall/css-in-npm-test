bb1b91f1c9fc1b4ea770105feea0d04fa26e3ebd
Peter Bagnall <peter.bagnall@ul.com>
2023-02-13 17:22:25 +0000

    build: move package_README.md to dist_README.md for clarity

--- scripts/publish.mjs
+++ scripts/publish.mjs
@@ -2,7 +2,7 @@
 import fs from 'fs';
 
 fs.copyFileSync("LICENSE", "dist/LICENCE");
-fs.copyFileSync("package_README.md", "dist/README.md");
+fs.copyFileSync("dist_README.md", "dist/README.md");
 const pkg = JSON.parse(fs.readFileSync("package.json", 'utf8'));
 
 const excludePackageKeys = new Set([
